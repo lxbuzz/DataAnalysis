e7b1df82bc9a4eddd826dfdb4dc9f94d3c27d908
Alex Bakun <ab@mediaplan.me>
2025-02-12 22:19:35 +0100

    restructure asset root folder

--- ScriptHW1.sql
+++
@@ -1,3 +0,0 @@
--- select * from facebook_ads_basic_daily fabd ;
--- select ad_date, spend, clicks, spend/clicks as cpc from facebook_ads_basic_daily fabd where clicks >0;
-select ad_date, spend, clicks, spend/clicks as cpc from facebook_ads_basic_daily fabd where clicks >0 order by ad_date desc;
